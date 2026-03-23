d935a7da3eb82cb0b9e84316a32ff5187267d5a3
angiebel26 <angievaleriabeltranreyes4@gmail.com>
2026-03-22 22:10:49 -0500

    Agregando estructura datos

--- Java11/Basicos java_11/Estructura Datos/estructuradatos.js
+++ Java11/Basicos java_11/Estructura Datos/estructuradatos.js
@@ -2,15 +2,49 @@
 
 //& Arrays
 
-let learness = ["Robinson", "Angie", "Hernan"];
+let learners = ["Ana", "Luis", "Carlos", "María"];
 
-console.log(learness[0]);  'Robinson'
-console.log(learness[1]);   'Angie'
-console.log(learness[2]);   'Hernan'
-console.log(learness[3]);   undefined 
+// Ahora sí puedes acceder a sus elementos
+console.log(learners[0]); // Ana
+console.log(learners[1]); // Luis
+console.log(learners[2]); // Carlos
+console.log(learners[3]); // María
 
-console.log(learners.length);
+// Y también a su longitud
+console.log(learners.length); // 4
 
-//! añadir un elemento
-console.log(learners.length); 
+//! añadir elemento
+learners.push("Juanda");
+console.log(learners);
+
+//! borrar
+learners.pop();
+console.log(learners);
+
+//!Buscar un elemento
+console.log(learners.includes("Juanda"));
+
+let carrito = [];
+let productos = ["Audifonos", "Teclado"]
+
+//! Objectos
+
+let estudiantes = {
+    nombre: "Pablo",
+    telefono: 12531456,
+    ciudad: "Bogota",
+    estactivo: true,
+    hobbies: ["Jugar","Programar","Escuchar musica"]
+}
+
+console.log(estudiantes.nombre);
+console.log(estudiantes["ciudad"])
+
+
+estudiantes.telefono = 23456212;
+estudiantes.hobbies.push("Leer");
+console.log(estudiantes)
+
+console.log(Object.keys(estudiantes));
+console.log(Object.keys(estudiantes)); 
 
